1d5e3d4d8ec5ecb98dd93686e1788e06c150ad92
jeevanm-02-06 <jeevanm1225@gmail.com>
2026-04-05 15:30:02 +0530

    plm1

---
+++ assignment 3/problem3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("Enter you no:");
+    scanf("%d",&x);
+    if (x<50) printf("Success");
+    else printf("Failiure");
+    
+    
+
+    return 0;
+}

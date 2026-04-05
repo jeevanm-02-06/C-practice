58a0de6f4e9d96ebea5db0871ace558f2da1a787
jeevanm-02-06 <jeevanm1225@gmail.com>
2026-04-05 16:03:51 +0530

    plm1

---
+++ assignment4/problem5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int x=0,y=0;
+   
+  loop: if(x<10) {\
+       if(x%2!=0){
+       printf("%d\n",x);
+       }
+       
+       x++;
+
+       
+       
+       
+       goto loop;
+   }
+   
+   
+    return 0;
+}
